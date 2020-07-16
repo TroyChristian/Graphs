1e02ea7a8beb98f11a9dac5f64abbb89460f89e3
Troy Christian <apostasy8@gmail.com>
2020-07-15 20:51:02 -0400

    islands complete

--- objectives/islands.py
+++ objectives/islands.py
@@ -13,8 +13,25 @@ islands = [[0, 1, 0, 1, 0],
 
 def get_neighbors(node, matrix):
     ## take a step north, south, east, west
+    row, col = node
+    stepNorth = stepSouth = stepWest = stepEast = False
     if row > 0:
         stepNorth = row - 1
+    if row < len(matrix) - 1:
+        stepSouth = row + 1
+    if col < len(matrix[row]) - 1:
+        stepEast = col + 1
+    if col > 0:
+        stepWest = col - 1 
+    
+    if stepNorth is not False and matrix[stepNorth][col] == 1:
+        neighbors.append((stepNorth, col))
+    if stepSouth is not False and matrix[stepSouth][col] == 1:
+        neighbors.append((stepSouth, col))
+    if stepEast is not False and matrix[row][stepEast] == 1:
+        neighbors.append((row, stepEast))
+    if stepWest is not False and matrix[row][stepWest] == 1:
+        neighbors.append((row, stepWest))
     
 
     
@@ -26,7 +43,7 @@ def dft_recursive(node):
 
 
         ###get neighbors
-        neighbors = get_neighbors(node)
+        neighbors = get_neighbors(node, matrix)
 
 
         ### for each neighbor
@@ -34,18 +51,22 @@ def dft_recursive(node):
 
 
         ### recurse
-        dft_recursive(neighbor, visited, matrix)
+            dft_recursive(neighbor, visited, matrix)
 
 
 def islands_counter(isles):
-    for row in range(len(isles):
+    visited = set()
+    number_islands = 0
+
+
+    for row in range(len(isles)):
         for col in range(len(isles[row])):
             node = (row, col )
 
             if node not in visited and isles[row][col] == 1:
                 number_islands += 1
 
-                dft_recursive(node)
+                dft_recursive(node, visited, isles)
             
     return number_islands
 
